f97ce44c50deb9c09ef46acc8a42cfda98e8e1dc
Jorge <jorgediazok@gmail.com>
2020-10-16 01:47:30 -0300

    inserted modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React,{useState, useEffect} from 'react';
+import React,{ useState, useEffect } from 'react';
 import Post from "./components/Post"
-import {db} from "./firebase"
+import { db } from "./firebase"
+import { makeStyles } from "@material-ui/core/styles"
+import Modal from "@material-ui/core/Modal"
 import './App.css';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
 
-  const [posts, setPosts] = useState([])
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle)
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false)
 
   useEffect(()=>{
     db.collection("posts").onSnapshot(snapshot=>{
-      setPosts(snapshot.docs.map(doc => doc.data()))
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   },[])
 
+
   return (
     <div className="app">
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
       </div>
-      {posts.map(post=>(
-        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+      
+      <Button onClick={()=>setOpen(true)}>Sign Up</Button>
+
+      {posts.map(({id,post}) => (
+        <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
       ))}
 
     </div>
