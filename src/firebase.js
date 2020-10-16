2aa7fea12ed6d970095b6a207f142442180f36f4
Jorge <jorgediazok@gmail.com>
2020-10-16 00:33:29 -0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase"
 
-var firebaseConfig = {
+const firebaseApp = firebase.initializeApp ({
   apiKey: "AIzaSyBV4h_APLvwCRTeKrT56QfR5Yd18fBL-us",
   authDomain: "instagram-clone-15aee.firebaseapp.com",
   databaseURL: "https://instagram-clone-15aee.firebaseio.com",
@@ -8,6 +8,10 @@ var firebaseConfig = {
   storageBucket: "instagram-clone-15aee.appspot.com",
   messagingSenderId: "513104316817",
   appId: "1:513104316817:web:85b5c260dfadc77a4d7230"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+});
+
+const db =  firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export {db, auth, storage};
