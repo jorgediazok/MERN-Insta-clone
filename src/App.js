9dd9b9a87dfb407f90bc017d0f368d1a28d85db3
Jorge <jorgediazok@gmail.com>
2020-10-15 23:37:24 -0300

    styled the post component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
       </div>
-      <h1>HELLO</h1>
       <Post/>
+      <Post/>
+      <Post/>
+
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Avatar from "@material-ui/core/Avatar"
 import "../styles/Post.css"
 
 function Post() {
   return (
-    <div>
-    <h3>Username</h3>
+    <div className="post">
+      <div className="post__header">
+      <Avatar className="post__avatar" alt="jorgediazok" src="/static/images/avatar/1.jpg" />
+      <h3>Username</h3>  
+      </div>      
+    <img className="post__image" src="https://edteam-media.s3.amazonaws.com/blogs/original/3a0fa242-1b90-42bf-831d-5ea432759ab0.jpg" alt="react"/>
+    <h4 className="post__text"><strong>jorgediazok</strong> Learning React is easy 🚀 </h4>
     </div>
   )
 }
