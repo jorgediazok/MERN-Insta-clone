4db101382fcfcde1f25416779a989a4b15d3c48b
Jorge <jorgediazok@gmail.com>
2020-10-16 00:03:32 -0300

    introduced state posts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Post from "./components/Post"
 import './App.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      username:"jorgediazok",
+      caption:"Learning React is really simple 🚀", 
+      imageUrl:"https://edteam-media.s3.amazonaws.com/blogs/original/3a0fa242-1b90-42bf-831d-5ea432759ab0.jpg"
+    },
+    {
+      username:"jorgediazok",
+      caption:"Learning Angular is not that simple 🐓",
+      imageUrl:"https://dmm40cf0lyret.cloudfront.net/wp-content/uploads/2020/08/Agencia-desarrollo-Angular-JS.jpg"
+    },
+    {
+      username:"jorgediazok",
+      caption:"Learning Vue is quiet ok 🏃",
+      imageUrl:"https://victorroblesweb.es/wp-content/uploads/2017/03/vuejs2-victorroblesweb.jpg"
+    }
+
+  ])
+
   return (
     <div className="app">
       <div className="app__header">
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
       </div>
-      <Post/>
-      <Post/>
-      <Post/>
+      {posts.map(post=>(
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+      ))}
 
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Avatar from "@material-ui/core/Avatar"
 import "../styles/Post.css"
 
-function Post() {
+function Post({imageUrl, caption, username}) {
+
+  
   return (
     <div className="post">
       <div className="post__header">
       <Avatar className="post__avatar" alt="jorgediazok" src="/static/images/avatar/1.jpg" />
-      <h3>Username</h3>  
+      <h3>{username}</h3>  
       </div>      
-    <img className="post__image" src="https://edteam-media.s3.amazonaws.com/blogs/original/3a0fa242-1b90-42bf-831d-5ea432759ab0.jpg" alt="react"/>
-    <h4 className="post__text"><strong>jorgediazok</strong> Learning React is easy 🚀 </h4>
+    <img className="post__image" src={imageUrl} alt="react"/>
+    <h4 className="post__text"><strong>{username} </strong>{caption}</h4>
     </div>
   )
 }
